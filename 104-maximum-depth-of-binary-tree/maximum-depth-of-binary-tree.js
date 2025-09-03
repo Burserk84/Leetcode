801fa13a295494612aa8849c1bba8bd1f924e4f7
Sharifiasldev <amirburserk@gmail.com>
2025-09-03 12:48:56 +0330

    Time: 1 ms (46.42%) | Memory: 57.3 MB (64.35%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.js
@@ -1,3 +1,15 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ * this.val = (val===undefined ? 0 : val)
+ * this.left = (left===undefined ? null : left)
+ * this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
 var maxDepth = function(root) {
     // Base case: If the node is null, we've reached the end of a path.
     // The depth of a null node is 0.
